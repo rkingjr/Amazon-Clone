49b9b6ddc45c1aac5304e27fe8a6bbbaad739ccf
Randy King Jr <rkingjr@outlook.com>
2022-04-20 17:24:45 -0500

    move files to root directory

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Hello, Randy. Let's build the Amazon Store!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCY889XfBPUpRU7oZoTr2YodmSZJz1eVvA",
+    authDomain: "clone-e6c9e.firebaseapp.com",
+    projectId: "clone-e6c9e",
+    storageBucket: "clone-e6c9e.appspot.com",
+    messagingSenderId: "868485151138",
+    appId: "1:868485151138:web:61b99c58e341c645ac9afb",
+    measurementId: "G-6XSD91QL3M"
+  };
